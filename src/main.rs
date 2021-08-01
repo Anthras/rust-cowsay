50c544abf821238f5835f6f2331a41e521f8c350
Anthras <weavertodda@gmail.com>
2021-08-01 14:37:58 -0500

    trying out Clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 use std::env;
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(name = "cowsay")]
+struct Message {
+    /// Message for ascii character to say
+    #[clap(short, long)]
+    msg: String,
+
+    /// Number of lines for speech bubble
+    #[clap(short, long, default_value = "1")]
+    number_of_lines: u8,
+
+    /// Ascii character
+    #[clap(short, long, default_value = "cow")]
+    ascii_character: String,
+}
+
 
 fn main() {
+    let cowsay = Message::parse();
+    println!("{}", cowsay.msg);
+
+
+
+
     // collect the arguments as a string from command line
-    let args: Vec<String> = env::args().collect();
+    /* let args: Vec<String> = env::args().collect(); */
     
     // for now lets have the usage be to only accept one string.
     // args[0] is the name of the program and each argument 
@@ -16,14 +40,14 @@ fn main() {
     // args[3] = "is"
     // args[4] = "a"
     // args[5] = "test"
-    let message = args[1].clone();
+    // message = args[1].clone();
 
     // so usage ` ./rust-cowsay "hello world!" `
+    /*
     if args.len() > 2 {
         println!("usage:  ./rust-cowsay \"hello world!\"");
     } else {
         println!("{}", &message);
-    }
-
+    } */
     
 }
